7911eda355ed686ce0343924cd4747441c81028b
Lucas Johann <gabif.johann@gmail.com>
2025-02-27 15:30:37 -0300

    price card - style

--- aula_04/price-card/src/components/card/index.jsx
+++ aula_04/price-card/src/components/card/index.jsx
@@ -1,16 +1,38 @@
-import "./style.css";
+import { Button } from "../button";
+import { ItemList } from "../item-list";
+import "./styles.css";
 
 export function Card({ description, title, price }) {
-  return (
-    <div className="container">
-      <p>{description}</p>
+  const ultimate = [
+    "usuários ilimitados",
+    "suporte 24/7",
+    "CSM dedicado",
+    "treinamentos",
+  ];
 
-      <div>
+  return (
+    <div className="container darkContainer">
+      <p id="description">{description}</p>
+      <div className="details">
         <h1>{title}</h1>
-        <span>
-          R$ <strong>{price}/mês</strong>
-        </span>
+        <p>
+          R$
+          <strong>
+            {price}
+            <span>/mês</span>
+          </strong>
+        </p>
       </div>
+
+      <Button title="Assinar agora" />
+
+      <hr />
+
+      <ul>
+        {ultimate.map((item, index) => (
+          <ItemList key={index} functionality={item} />
+        ))}
+      </ul>
     </div>
   );
 }
