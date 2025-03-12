c44893272137d40d972b70de7ed5de15d3df8b9c
Lucas Johann <gabif.johann@gmail.com>
2025-03-12 15:11:40 -0300

    correcao prova turma 5A

---
+++ template-prova-5a/src/components/card/index.jsx
@@ -0,0 +1,35 @@
+import { ShoppingCart } from "lucide-react";
+import "./styles.css";
+
+export function Card({ imgCoffee, tags, title, description }) {
+  return (
+    <div id="card">
+      <img src={imgCoffee} alt="" className="coffeeImg" />
+
+      <div id="tags">
+        {tags.map((tag, index) => (
+          <span>{tag}</span>
+        ))}
+      </div>
+
+      <h1>{title}</h1>
+
+      <p className="description">{description} </p>
+
+      <div id="details">
+        <span>
+          R$ <strong>9,90</strong>
+        </span>
+
+        <div className="quantityButtons">
+          <button type="button">-</button>
+          <p>1</p>
+          <button type="button">+</button>
+        </div>
+        <button type="button" className="cartBtn">
+          <ShoppingCart size={22} color="#F3F2F2" fill="#F3F2F2" />
+        </button>
+      </div>
+    </div>
+  );
+}
