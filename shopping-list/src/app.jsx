0dce6ba4729db28f898d490e0df39f0ea376ff0a
Lucas Johann <gabif.johann@gmail.com>
2025-04-08 15:37:25 -0300

    shooping list - add produto

--- shopping-list/src/app.jsx
+++ shopping-list/src/app.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Header } from "./components/header";
 import { Form } from "./components/form";
 import { Product } from "./components/product";
@@ -5,24 +6,43 @@ import { Product } from "./components/product";
 import shoopingList from "../data.json";
 
 export function App() {
+  const [productList, setProductList] = useState(shoopingList.lista_de_compras);
+
+  function handleAddProcut(newProduct) {
+    setProductList((prevList) => [...prevList, newProduct]);
+  }
+
+  /*   
+  const [search, setSearch] = useState("");
+
+  const filteredList = shoopingList.lista_de_compras.filter(({ nome }) =>
+    nome.toLocaleLowerCase().includes(search.toLocaleLowerCase)
+  ); */
+
   return (
     <>
       <Header />
       <div className="container">
-        <Form />
+        {/*   <Form onSearch={setSearch} /> */}
+        <Form onAddProduct={handleAddProcut} />
 
         <div className="product-list">
-          {shoopingList.lista_de_compras.map(
-            /* desestruturação */
-            ({ nome, quantidade, tipo }, index) => (
-              <Product
-                key={index}
-                productName={nome}
-                quantity={quantidade}
-                type={tipo}
-              />
-            )
-          )}
+          {productList.map(({ nome, quantidade, tipo }, index) => (
+            <Product
+              key={index}
+              productName={nome}
+              quantity={quantidade}
+              type={tipo}
+            />
+          ))}
+          {/*     {filteredList.map(({ nome, quantidade, tipo }, index) => (
+            <Product
+              key={index}
+              productName={nome}
+              quantity={quantidade}
+              type={tipo}
+            />
+          ))} */}
         </div>
       </div>
     </>
--- shopping-list/src/components/form/index.jsx
+++ shopping-list/src/components/form/index.jsx
@@ -1,19 +1,64 @@
+import { useState } from "react";
 import { Plus } from "lucide-react";
 import "./styles.css";
 
-export function Form() {
+/* export function Form({ onSearch }) { 
+  const [input, setInput] = useState("");
+
+function handleChange(e) {
+  const value = e.target.value;
+  setInput(value);
+  onSearch(value); // chamando a função recebida como prop
+  } */
+
+export function Form({ onAddProduct }) {
+  const [nome, setNome] = useState("");
+  const [quantidade, setQuantidade] = useState(1);
+  const [tipo, setTipo] = useState("un.");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (nome.trim() === "") return;
+
+    onAddProduct({
+      nome,
+      quantidade,
+      tipo,
+    });
+
+    setNome("");
+    setQuantidade(1);
+    setTipo("un.");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="item-input">
         <label htmlFor="item">Item</label>
-        <input type="text" className="item" />
+        <input
+          type="text"
+          className="item"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
       </div>
 
       <div className="item-input">
         <label htmlFor="quantity">Quantidade</label>
         <div className="quantity-input-container">
-          <input type="number" className="quantity-input" min={1} />
-          <select className="unit-select">
+          <input
+            type="number"
+            className="quantity-input"
+            min={1}
+            value={quantidade}
+            onChange={(e) => setQuantidade(Number(e.target.value))}
+          />
+          <select
+            className="unit-select"
+            value={tipo}
+            onChange={(e) => setTipo(e.target.value)}
+          >
             <option value="un.">Un.</option>
             <option value="L">L</option>
             <option value="Kg">Kg</option>
@@ -25,7 +70,7 @@ export function Form() {
         <label htmlFor="category">Categoria</label>
       </div>
 
-      <button type="button">
+      <button type="submit">
         <Plus size={24} />
       </button>
     </form>
